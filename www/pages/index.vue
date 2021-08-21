9c9eee200520b0e358813d2d5679156f4dfb966b
MoneyIgos <makowskiigor@gmail.com>
2021-08-22 00:19:46 +0200

    refactor(index): add flex-wrap

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <shop-item title='Aligator Haze' price=69 imgSrc='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    </v-col>
-  </v-row>
+  <v-container class="d-flex flex-wrap" height="100">
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4'/>
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+  </v-container>
 </template>
-
