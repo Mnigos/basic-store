decae7dd4c62831f67155ba44d8c2b5987fdf48f
jakubek0708 <jakubek0708@gmail.com>
2021-08-22 00:34:05 +0200

    refactor: create v-for for shop items

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,16 +1,24 @@
 <template>
   <v-container class="d-flex flex-wrap" height="100">
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4'/>
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
-    <shop-item title='Aligator Haze' price=69 img-src='https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4' />
+    <div v-for='({title, price, imgSrc}, i) in items' :key='i'>
+      <shop-item :title='title' :price='price' :img-src='imgSrc'/>
+    </div>
   </v-container>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => ({
+    items: [{
+      title: 'Aligator Haze',
+      price: 69,
+      imgSrc: 'https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4'
+    }, {
+      title: 'Mnigos Haze',
+      price: 420,
+      imgSrc: 'https://avatars.githubusercontent.com/u/56691628?v=4'
+    }]
+  })
+})
+</script>
