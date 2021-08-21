11f16d97d394196b0ad118b7c7c1f5c792f0b1e3
MoneyIgos <makowskiigor@gmail.com>
2021-08-22 00:40:49 +0200

    refactor(index): add margin to shop-item component

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="d-flex flex-wrap" height="100">
-    <div v-for='({title, price, imgSrc}, i) in items' :key='i'>
+    <div v-for='({title, price, imgSrc}, i) in items' :key='i' class="ma-5">
       <shop-item :title='title' :price='price' :img-src='imgSrc'/>
     </div>
   </v-container>
@@ -10,15 +10,23 @@
 import Vue from 'vue'
 export default Vue.extend({
   data: () => ({
-    items: [{
-      title: 'Aligator Haze',
-      price: 69,
-      imgSrc: 'https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4'
-    }, {
-      title: 'Mnigos Haze',
-      price: 420,
-      imgSrc: 'https://avatars.githubusercontent.com/u/56691628?v=4'
-    }]
+    items: [
+      {
+        title: 'Aligator Haze',
+        price: 69,
+        imgSrc: 'https://a.allegroimg.com/s1024/0cce55/71e4c3b240d688d3cf0b084567e4',
+      },
+      {
+        title: 'Mnigos Haze',
+        price: 420,
+        imgSrc: 'https://avatars.githubusercontent.com/u/56691628?v=4',
+      },
+      {
+        title: 'ŻUBRERK ija',
+        price: 2390,
+        imgSrc: 'https://avatars.githubusercontent.com/u/70911746?v=4',
+      }
+    ]
   })
 })
 </script>
